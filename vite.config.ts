b9279276f051ae2c401ea6570ba2c3c7c8458def
tithepmostasis-ux <tithepmostasis@gmail.com>
2026-06-18 22:41:12 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/conhienkhtn/',
+    base: '/khtnconhien/',
 
     server: {
       port: 3000,
